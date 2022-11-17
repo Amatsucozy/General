54a32aefc69c657da0931d5579417983ab8e70c5
Amatsucozy <amatsucozy@gmail.com>
2022-11-17 23:46:05 +0700

    Added value comparer for EventTemplate.Parameters conversion

--- Florence/Florence.EF/EntityConfig/EventTemplateConfig.cs
+++ Florence/Florence.EF/EntityConfig/EventTemplateConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using Florence.Domain.EventTemplate.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Florence.EF.EntityConfig;
@@ -8,6 +9,12 @@ namespace Florence.EF.EntityConfig;
 public sealed class EventTemplateConfig : IEntityTypeConfiguration<EventTemplate>
 {
     private static readonly JsonSerializerOptions JsonSerializerOptions = new();
+
+    private static readonly ValueComparer ParametersValueComparer = new ValueComparer<string[]>(
+        (first, second) => (first ?? Array.Empty<string>()).SequenceEqual(second ?? Array.Empty<string>()),
+        array => array.GetHashCode(),
+        array => array.Select(arrayElement => arrayElement).ToArray()
+    );
 
     public void Configure(EntityTypeBuilder<EventTemplate> builder)
     {
@@ -22,7 +29,8 @@ public sealed class EventTemplateConfig : IEntityTypeConfiguration<EventTemplate
             .HasConversion(
                 parameters => JsonSerializer.Serialize(parameters, JsonSerializerOptions),
                 serialized => JsonSerializer.Deserialize<string[]>(serialized, JsonSerializerOptions) ??
-                              Array.Empty<string>()
+                              Array.Empty<string>(),
+                ParametersValueComparer
             );
     }
 }
